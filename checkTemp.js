a6fdcc2968298043012d466022be8886a85b3838
Aurelia M <77746892+awrelyah@users.noreply.github.com>
2022-01-07 09:03:35 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -2,13 +2,13 @@ let temperature = prompt("Sisesta katla temperatuur (0-60kraadi)");
 
 function checkTemp(temperature){
     if (temperature >= 0 && temperature <= 20){
-        alert(-1);
+        return(-1);
     }
     else if (temperature > 20 && temperature <= 40){
-        alert(0);
+        return(0);
     }
     else if (temperature > 40 && temperature <= 60){
-        alert(1);
+        return(1);
     }
 }
 
